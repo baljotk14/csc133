64d9def15666f6f4a42389b6ade9dd670016cd38
baljotk <baljotkaur@csus.edu>
2024-10-08 21:56:37 -0700

    initialize opengl and window

--- src/main/java/pkgSlUtils/slWindowManager.java
+++ src/main/java/pkgSlUtils/slWindowManager.java
@@ -5,50 +5,69 @@ import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class slWindowManager {
 
-    private static long win_id = 0;
-
-
-    private static slWindowManager mywm = null;
-
+    private long glfw_win = 0;
+    private static slWindowManager my_window = null;
+    private int[] windowSize = new int[2];
 
     private slWindowManager() {}
 
-
     public static slWindowManager get() {
-        if (mywm == null) {
-            mywm = new slWindowManager();
+        if (my_window == null) {
+            my_window = new slWindowManager();
         }
-        return mywm;
+        return my_window;
     }
 
-
-    public void initGLFWWindow(int width, int height, String label) {
-        if (win_id == 0) {
-            // Initialize GLFW
+    public long initGLFWWindow(int width, int height, String label) {
+        if (glfw_win == 0) {
             if (!glfwInit()) {
                 throw new IllegalStateException("Unable to initialize GLFW");
             }
 
-
-            win_id = glfwCreateWindow(width, height, label, NULL, NULL);
-            if (win_id == NULL) {
+            glfw_win = glfwCreateWindow(width, height, label, NULL, NULL);
+            if (glfw_win == NULL) {
                 throw new RuntimeException("Failed to create the GLFW window");
             }
 
-
-            glfwMakeContextCurrent(win_id);
+            glfwMakeContextCurrent(glfw_win);
+            windowSize[0] = width;
+            windowSize[1] = height;
         }
+        return glfw_win;
     }
 
-
-    public void makeContextCurrent() {
-        if (win_id != 0) {
-            glfwMakeContextCurrent(win_id);
-        }
+    public int[] getWindowSize() {
+        return windowSize;
     }
-
 
     public long getWindowID() {
-        return win_id;
+        return glfw_win;
     }
+
+    public void updateContextToThis() {
+        if (glfw_win != 0) {
+            glfwMakeContextCurrent(glfw_win);
+        }
+    }
+
+    public void swapBuffers() {
+        if (glfw_win != 0) {
+            glfwSwapBuffers(glfw_win);
+        }
+    }
+
+    public boolean isGLFWWindowClosed() {
+        return glfwWindowShouldClose(glfw_win);
+    }
+
+//    public void destroyGlfwWindow() {
+//        if (glfw_win != 0) {
+//            glfwDestroyWindow(glfw_win);
+//            glfw_win = 0;
+//        }
+//    }
+//
+//    public int[] getCurrentWindowSize() {
+//        return new int[]{windowSize[0], windowSize[1]};
+//    }
 }
