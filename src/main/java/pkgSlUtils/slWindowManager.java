073d9667f97d0a6ca5256c2753c651a8ca2c2254
baljotk <baljotkaur@csus.edu>
2024-10-08 22:23:39 -0700

    returns current window size in array

--- src/main/java/pkgSlUtils/slWindowManager.java
+++ src/main/java/pkgSlUtils/slWindowManager.java
@@ -6,7 +6,7 @@ import static org.lwjgl.system.MemoryUtil.NULL;
 public class slWindowManager {
 
     private long glfw_win = 0;
-     private static slWindowManager my_window = null;
+    private static slWindowManager my_window = null;
     private int[] windowSize = new int[2];
 
     private slWindowManager() {}
@@ -60,14 +60,14 @@ public class slWindowManager {
         return glfwWindowShouldClose(glfw_win);
     }
 
-//    public void destroyGlfwWindow() {
-//        if (glfw_win != 0) {
-//            glfwDestroyWindow(glfw_win);
-//            glfw_win = 0;
-//        }
-//    }
-//
-//    public int[] getCurrentWindowSize() {
-//        return new int[]{windowSize[0], windowSize[1]};
-//    }
+    public void destroyGlfwWindow() {
+        if (glfw_win != 0) {
+            glfwDestroyWindow(glfw_win);
+            glfw_win = 0;
+        }
+    }
+
+    public int[] getCurrentWindowSize() {
+        return new int[]{windowSize[0], windowSize[1]};
+    }
 }
